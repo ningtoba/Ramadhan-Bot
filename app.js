3aa5e1e8c7827fa17b99d105768a4310012f0c68
ichiroadris <ichiroadris@gmail.com>
2020-04-24 15:49:31 +0800

    feat: add command

--- app.js
+++ app.js
@@ -9,6 +9,24 @@ bot.command("ping", (msg, reply, next) => {
   reply.text("hai nabil")
 })
 
+bot.command("Khatam tracker", (msg, reply, next) => {
+    reply.text("Have you read your Quran yet?")
+})
+
+bot.command("Resepi hari ini", (msg, reply, next) => {
+    reply.text("Follow @khairulaming")
+})
+
+bot.command("Hadis hari ini", (msg, reply, next) => {
+    reply.text("Orang yang berpuasa sedangkan mereka tidak memelihara lidah (percakapannya) dan juga masih melakukan maksiat, tidak akan diberi pahala oleh Allah SWT ke atas puasa mereka selain lapar dan dahaga sahaja.")
+})
+
+bot.command("Solat Tracker", (msg, reply, next) => {
+    reply.text("Solat lah 5 waktu")
+})
+
+
 bot.text((msg, reply, next) => {
   console.log("Received a text message:", msg.text);
 });
+
