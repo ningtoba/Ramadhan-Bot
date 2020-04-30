47ad89e61a144cc67c9b57e900191ba0230120a0
Muhammad Luqman Hakim <jawbajaw40@gmail.com>
2020-04-30 23:51:11 +0800

    api get req commented

--- functions/Resepi.js
+++ functions/Resepi.js
@@ -1,16 +1,16 @@
 const https = require('https');
 
-https.get('https://api.spoonacular.com/recipes/random?number=1&apiKey=c5df1ded17d348fab88dc23a4fb1fc80', res => {
-    let data = ''
+// https.get('https://api.spoonacular.com/recipes/random?number=1&apiKey=c5df1ded17d348fab88dc23a4fb1fc80', res => {
+//     let data = ''
 
-    res.on('data', chunk => {
-        data += chunk;
-    })
+//     res.on('data', chunk => {
+//         data += chunk;
+//     })
 
-    res.on('end', () => {
-        var url = JSON.parse(data)
-    })
-})
+//     res.on('end', () => {
+//         var url = JSON.parse(data)
+//     })
+// })
 
 var methods = {
     starter(msg, reply) {
